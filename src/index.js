44dce4947e06392a9b8488c18182548117eb3ccb
Tran Quang Huy <huytq@nal.vn>
2023-05-03 00:03:37 +0700

    config react app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Tạo component App
 function App() {
@@ -7,6 +7,6 @@ function App() {
     <div>
       <h1>Xin chào anh em F8!</h1>
     </div>
-  );
+  )
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,72 @@ const path = require("path");
 /* thêm html-webpack-plugin vào file cấu hình */
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  /* đây là file đầu tiên mà webpack sẽ đọc ở đây mình để index.js */
-  entry: path.resolve(__dirname, "./src/index.js"),
-  /* cấu hình thư mục đầu ra là dist và tên file là index.bundle.js,
-  clean dùng để reset thư mục dist khi build */
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-    clean: true,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+module.exports = () => {
+  const IS_DEV = true;
+
+  return {
+    /* đây là file đầu tiên mà webpack sẽ đọc ở đây mình để index.js */
+
+    mode: IS_DEV ? "development" : "production",
+    /* cho biết vị trí chính xác của lỗi */
+    watch: true,
+    /* cho biết vị trí chính xác của lỗi */
+    devtool: IS_DEV ? "eval-source-map" : "source-map",
+    entry: path.resolve(__dirname, "./src/index.js"),
+    /* cấu hình thư mục đầu ra là build và tên file là random,
+    clean dùng để reset thư mục dist khi build */
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].js",
+      clean: true,
     },
-  },
-  /* đoạn code sau sẽ load các gói babel vào webpack */
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
-        exclude: /node_modules/, // Loại trừ thư mục node_modules
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    /* đoạn code sau sẽ load các gói babel vào webpack */
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/, // Sẽ sử dụng babel-loader cho những file .ts || .tsx
+          exclude: /node_modules/,
+          use: ["babel-loader"], // Giúp dịch code TS, React sang JS,
+        },
+        {
+          test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
+          exclude: /node_modules/, // Loại trừ thư mục node_modules
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
-        use: ["style-loader", "css-loader"],
-      },
+        {
+          test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(jpg|jpeg|png|svg|woff|eot|ttf|otf|pdf|gif)$/,
+          use: ["file-loader"],
+        },
+      ],
+    },
+    /* cấu hình file index.html từ folder public */
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
     ],
-  },
-  /* cấu hình file index.html từ folder public */
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
-  /* tự mở tab trình duyệt mới */
-  devServer: {
-    open: true,
-  },
-  mode: "development",
+    /* tự mở tab trình duyệt mới */
+    devServer: {
+      open: true,
+    },
+
+    //tắt gợi ý performance, cần nghiên cứu thêm cái qq này
+    performance: {
+      hints: false,
+    },
+  };
 };
