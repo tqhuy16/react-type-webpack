45310b1666876e3930250ff389427e71237c71e3
TQHuy <tqhuy16@gmail.com>
2023-05-31 22:11:43 +0700

    upload file not complete

--- src/component/UploadImage/upload-image.tsx
+++ src/component/UploadImage/upload-image.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Upload } from 'antd'
-import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface'
+import type { RcFile, UploadChangeParam, UploadFile, UploadProps } from 'antd/es/upload/interface'
 import { Control, Controller, FieldValues, Path } from 'react-hook-form'
 
 import styles from './uploadImage.module.scss'
@@ -10,30 +10,38 @@ interface IUploadImageProps<T extends FieldValues> {
   control: Control<T>
   label?: string
   className?: string
+  maxCount?: number
 }
 
-const UploadImage = <T extends FieldValues>({ name, control, label, className, ...rest }: IUploadImageProps<T>) => {
+const UploadImage = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  className,
+  maxCount = 3,
+  ...rest
+}: IUploadImageProps<T>) => {
   const [fileList, setFileList] = useState<UploadFile[]>([])
 
-  const onChange: UploadProps['onChange'] = ({ fileList: newFileList }) => {
-    console.log('newFileList', newFileList)
-    setFileList(newFileList)
-  }
+  // const onChange: UploadProps['onChange'] = ({ fileList: newFileList }) => {
+  //   console.log('newFileList', newFileList)
+  //   setFileList(newFileList)
+  // }
 
-  const propsImage: UploadProps = {
-    // onRemove: (file) => {
-    //   const index = fileList.indexOf(file)
-    //   const newFileList = fileList.slice()
-    //   newFileList.splice(index, 1)
-    //   setFileList(newFileList)
-    // },
-    beforeUpload: (file) => {
-      setFileList([...fileList, file])
+  // const propsImage: UploadProps = {
+  //   onRemove: (file) => {
+  //     const index = fileList.indexOf(file)
+  //     const newFileList = fileList.slice()
+  //     newFileList.splice(index, 1)
+  //     setFileList(newFileList)
+  //   },
+  //   beforeUpload: (file) => {
+  //     setFileList([...fileList, file])
 
-      return false
-    },
-    fileList
-  }
+  //     return false
+  //   },
+  //   fileList
+  // }
 
   const onPreview = async (file: UploadFile) => {
     let src = file.url as string
@@ -56,23 +64,19 @@ const UploadImage = <T extends FieldValues>({ name, control, label, className, .
       <Controller
         name={name}
         control={control}
-        render={({ field, fieldState: { error } }) => (
-          <>
-            <Upload
-              {...propsImage}
-              //   accept='image/png, image/jpeg'
-              //   action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
-              listType='picture-card'
-              fileList={fileList}
-              onChange={onChange}
-              onPreview={onPreview}
-              maxCount={3}
-              defaultFileList={[...fileList]}
-            >
-              {fileList.length < 3 && `+ Upload`}
-            </Upload>
-            {error?.message && <p className='text-error'>{error?.message}</p>}
-          </>
+        render={({ field: { value, onChange } }) => (
+          <Upload
+            // {...propsImage}
+            onChange={(info: UploadChangeParam) => {
+              onChange(info.fileList)
+            }}
+            fileList={value}
+            maxCount={maxCount}
+            listType='picture-card'
+            onPreview={onPreview}
+          >
+            {fileList.length < maxCount && '+ Upload'}
+          </Upload>
         )}
       />
     </>
--- src/shared/validation/example-form-validation.ts
+++ src/shared/validation/example-form-validation.ts
@@ -1,18 +1,18 @@
 import * as yup from 'yup'
 
 const exampleFormValidation = yup.object().shape({
-  firstName: yup.string().required('required'),
-  lastName: yup.string().required('required'),
-  firstAddress: yup.string().required('required'),
+  firstName: yup.string(),
+  lastName: yup.string(),
+  firstAddress: yup.string(),
   secondAddress: yup.string(),
-  gender: yup.string().required('required'),
-  checkbox: yup.array().min(1, 'minimum 1 item').required('required'),
+  gender: yup.string(),
+  checkbox: yup.array().min(1, 'minimum 1 item'),
   phone: yup.string(),
-  email: yup.string().required('required').email('email is not correct'),
+  email: yup.string().email('email is not correct'),
   // email: yup.string(),
-  position: yup.string().required('required'),
-  dateOfBirth: yup.string().required('required'),
-  description: yup.string().required('required')
+  position: yup.string(),
+  dateOfBirth: yup.string(),
+  description: yup.string()
   // avatar: ,
 })
 
